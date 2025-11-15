8bafa45925374b2a4361c36ef4506331731b958a
attil <attil@GEPATI>
2025-11-15 13:25:11 +0100

    Add Ionic frontend with books list

--- Library.Frontend/library-frontend/src/app/home/home.page.ts
+++ Library.Frontend/library-frontend/src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BooksService, Book } from '../services/books.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule, HttpClientModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  books: Book[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private booksService: BooksService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.booksService.getBooks().subscribe({
+      next: (data: Book[]) => {
+        this.books = data;
+        this.loading = false;
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.error = 'Failed to load books';
+        this.loading = false;
+      },
+    });
+  }
 }
